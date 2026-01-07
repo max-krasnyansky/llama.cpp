88a54c3bf547d7469bc430cbb6de15f219ee2aef
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-01-07 14:33:06 -0800

    hexagon: disable repack buffers if host buffers are disabled, improved handling of env vars

--- scripts/snapdragon/adb/run-bench.sh
+++ scripts/snapdragon/adb/run-bench.sh
@@ -34,13 +34,16 @@ nhvx=
 ndev=
 [ "$NDEV" != "" ] && ndev="GGML_HEXAGON_NDEV=$NDEV"
 
+hb=
+[ "$HB" != "" ] && hb="GGML_HEXAGON_HOSTBUF=$HB"
+
 set -x
 
 adb $adbserial shell " \
   cd $basedir;         \
   LD_LIBRARY_PATH=$basedir/$branch/lib   \
   ADSP_LIBRARY_PATH=$basedir/$branch/lib \
-    $ndev $nhvx $opmask $verbose $experimental $profile ./$branch/bin/llama-bench --device $device --mmap 0 -m $basedir/../gguf/$model \
+    $ndev $nhvx $opmask $verbose $experimental $profile $hb ./$branch/bin/llama-bench --device $device --mmap 0 -m $basedir/../gguf/$model \
         --poll 1000 -t 6 --cpu-mask 0xfc --cpu-strict 1 \
         --batch-size 128 -ngl 99 $cli_opts $@ \
 "
