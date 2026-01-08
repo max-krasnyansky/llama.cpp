af2d0e39dbcbe583cd823b9c67616c735d1036dd
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-01-08 15:54:46 -0800

    hexagon: just saving to rebase

--- ggml/src/ggml-hexagon/htp/cpy-ops.c
+++ ggml/src/ggml-hexagon/htp/cpy-ops.c
@@ -21,6 +21,21 @@
 #include "hvx-utils.h"
 #include "ops-utils.h"
 
+struct htp_copy_context {
+    struct htp_ops_context * octx;
+
+    uint32_t          src0_type_size;
+    uint32_t          src0_block_size;
+
+    uint32_t          dst_type_size;
+    uint32_t          dst_block_size;
+
+    uint32_t          src0_blocks_per_row;
+    uint32_t          dst_block_per_row;
+
+    void (*copy)(struct htp_copy_context * ct, int nth, int ith);
+};
+
 #define cpy_preamble \
     struct htp_tensor *src0 = &octx->src0; \
     struct htp_tensor *dst  = &octx->dst;  \
@@ -47,28 +62,14 @@
                                        \
     const uint32_t   nr = ne01;
 
-static int cpy_thread_f32_f32(struct htp_ops_context * octx, const int nth, const int ith) {
+static int cpy_thread_reshape_sametype(struct htp_copy_context * ct, int nth, int ith) {
+    struct htp_ops_context * octx = ct->octx;
     cpy_preamble;
 
     // parallelize by src0 rows
     const uint32_t dr  = octx->src0_nrows_per_thread;
     const uint32_t ir0 = dr * ith;
     const uint32_t ir1 = (ir0 + dr) < nr ? (ir0 + dr) : nr;
-
-    // row size equal
-    if (ne00 == ne0 && (nb00 == sizeof(float)) && (nb0 == sizeof(float))) {
-        // copy by rows
-        for (uint32_t i03 = 0; i03 < ne03; i03++) {
-            for (uint32_t i02 = 0; i02 < ne02; i02++) {
-                for (uint32_t i01 = ir0; i01 < ir1; i01++) {
-                    uint8_t* dst_ptr  = (uint8_t*) dst->data  + i01*nb1  + i02*nb2  + i03*nb3;
-                    uint8_t* src0_ptr = (uint8_t*) src0->data + i01*nb01 + i02*nb02 + i03*nb03;
-                    hvx_copy_fp32_uu(dst_ptr, src0_ptr, ne00);
-                }
-            }
-        }
-        return HTP_STATUS_OK;
-    }
 
     // dst counters
     int64_t k10 = 0;
@@ -77,8 +78,8 @@ static int cpy_thread_f32_f32(struct htp_ops_context * octx, const int nth, cons
     int64_t i13 = 0;
 
     // number of blocks in a row
-    const int64_t nk00 = ne00 / 1;
-    const int64_t nk0  = ne0  / 1;
+    const int64_t nk00 = ne00 / ct->block_size; 
+    const int64_t nk0  = ne0  / ct->block_size;
 
     for (int64_t i03 = 0; i03 < ne03; i03++) {
         for (int64_t i02 = 0; i02 < ne02; i02++) {
@@ -99,7 +100,7 @@ static int cpy_thread_f32_f32(struct htp_ops_context * octx, const int nth, cons
                 for (int64_t k00 = 0; k00 < nk00; k00++) {
                     const char * src0_ptr = ((char *) src0->data + k00*nb00 + i01*nb01 + i02*nb02 + i03*nb03);
                           char * dst_ptr  = ((char *)  dst->data + k10*nb0  + i11*nb1  + i12*nb2  + i13*nb3);
-                    memcpy(dst_ptr, src0_ptr, 4);
+                    memcpy(dst_ptr, src0_ptr, type_size);
 
                     if (++k10 == nk0) {
                         k10 = 0;
@@ -134,6 +135,27 @@ static int cpy_thread_f32_f32(struct htp_ops_context * octx, const int nth, cons
     return HTP_STATUS_OK;
 }
 
+static int cpy_thread_sametype_sameshape(struct htp_ops_context * octx, const int nth, const int ith) {
+    cpy_preamble;
+
+    // parallelize by src0 rows
+    const uint32_t dr  = octx->src0_nrows_per_thread;
+    const uint32_t ir0 = dr * ith;
+    const uint32_t ir1 = (ir0 + dr) < nr ? (ir0 + dr) : nr;
+
+    // copy by rows
+    for (uint32_t i03 = 0; i03 < ne03; i03++) {
+        for (uint32_t i02 = 0; i02 < ne02; i02++) {
+            for (uint32_t i01 = ir0; i01 < ir1; i01++) {
+                uint8_t* dst_ptr  = (uint8_t*) dst->data  + i01*nb1  + i02*nb2  + i03*nb3;
+                uint8_t* src0_ptr = (uint8_t*) src0->data + i01*nb01 + i02*nb02 + i03*nb03;
+                hvx_copy_fp32_uu(dst_ptr, src0_ptr, ne00);
+            }
+        }
+    }
+    return HTP_STATUS_OK;
+}
+
 static void cpy_work_f32_f32(unsigned int n, unsigned int i, void *data) {
     cpy_thread_f32_f32((struct htp_ops_context *) data, n, i);
 }
@@ -141,11 +163,20 @@ static void cpy_work_f32_f32(unsigned int n, unsigned int i, void *data) {
 int op_cpy(struct htp_ops_context * octx) {
     cpy_preamble;
 
-    if (octx->src0.type != HTP_TYPE_F32) {
+    struct htp_copy_context ct;
+    ct.octx = octx;
+
+    switch (src0->type) {
+    case HTP_TYPE_F32: ct.src0_type_size = 4; ct.src0_block_size = 1; ct.src0_bocks_per_row = ne00 / 1; break;
+    case HTP_TYPE_F16: ct.src0_type_size = 2; ct.src0_block_size = 1; ct.src0_bocks_per_row = ne00 / 1; break;
+    default:
         return HTP_STATUS_NO_SUPPORT;
     }
 
-    if (octx->dst.type != HTP_TYPE_F32) {
+    switch (dst->type) {
+    case HTP_TYPE_F32: ct.dst_type_size = 4; ct.dst_block_size = 1; ct.dst_bocks_per_row = ne0 / 1; break;
+    case HTP_TYPE_F16: ct.dst_type_size = 2; ct.dst_block_size = 1; ct.dst_bocks_per_row = ne0 / 1; break;
+    default:
         return HTP_STATUS_NO_SUPPORT;
     }
 
@@ -153,23 +184,17 @@ int op_cpy(struct htp_ops_context * octx) {
         return HTP_STATUS_OK;
     }
 
-    const bool reshape = (ne00 != ne0 || ne01 != ne1 || ne02 != ne2 || ne03 != ne3);
-    if (reshape) {
-        // Reshape mode needs different fastdivs
-        octx->flags |= 0x80000000;
-        octx->cpy_rshp_div_n0      = init_fastdiv_values(ne00);
-        octx->cpy_rshp_div_n1n0    = init_fastdiv_values(ne01 * ne00);
-        octx->cpy_rshp_div_n2n1n0  = init_fastdiv_values(ne02 * ne01 * ne00);
-    } else {
-        octx->cpy_div_ne01 = init_fastdiv_values(ne01);
-        octx->cpy_div_ne02 = init_fastdiv_values(ne02);
-        octx->cpy_div_ne03 = init_fastdiv_values(ne03);
-    }
+    const bool sametype  = (src0->type == dst->type);
+    const bool sameshape = (ne00 == ne0 && ne01 == ne1 && ne02 == ne2 && ne03 == ne3);
 
     const uint32_t n_jobs = MIN(nr, octx->n_threads);
     octx->src0_nrows_per_thread = (nr + n_jobs - 1) / n_jobs;
 
-    worker_pool_run_func(octx->ctx->worker_pool, cpy_work_f32_f32, octx, n_jobs);
+    if (sametype && sameshape) {
+        ct.copy = copy_thread_sametype_sameshape;
+
+
+    worker_pool_run_func(octx->ctx->worker_pool, cpy_work_func, octx, n_jobs);
 
     return HTP_STATUS_OK;
 }
